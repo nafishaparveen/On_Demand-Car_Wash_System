996f82d3dff1bf8cb11773ab40ee134d70db3b04
Nafisha Parveen <82298270+nafishaparveen@users.noreply.github.com>
2021-06-28 20:31:09 +0530

    Add files via upload

---
+++ admin-service/src/main/java/com/javabrains/adminservice/AdminServiceApplication.java
@@ -0,0 +1,23 @@
+package com.javabrains.adminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServiceApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+}
